da65042459b615ee82a5b7d46e18b2434b85d997
Gurleen Kaur <gurleenkaurgkp@gmail.com>
2024-12-30 12:41:14 +0530

    about and contact page

---
+++ assets/js/function.js
@@ -0,0 +1,14 @@
+// // Ensure dropdown works properly on hover
+// $('.nav-item.dropdown').hover(
+//     function () {
+//         $(this).addClass('show');
+//         $(this).find('.dropdown-menu').addClass('show');
+//     },
+//     function () {
+//         $(this).removeClass('show');
+//         $(this).find('.dropdown-menu').removeClass('show');
+//     }
+// );
+// $('.dropdown-toggle').click(function (e) {
+//     e.preventDefault();
+// });
